8152ab8125058267fc8f451274b057ec5b943546
Memitaru <amandatwah@gmail.com>
2020-01-10 19:15:00 -0800

    Finished names.py by creating a Binary Search Tree

--- names/names.py
+++ names/names.py
@@ -1,5 +1,50 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        # check if new value is less than current node
+        if value < self.value:
+            # is there already a value at self.left
+            # if there is no self.left value
+            if not self.left:
+                # set the new left child to a new value
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        # the new value is greater than the current node
+        # go right
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # if the root node is the target value, we found the value
+        if self.value == target:
+            return True
+        # target is smaller, go left
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        # target is greater, go right
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,11 +55,25 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Create a binary search tree with the first name
+bst = BinarySearchTree(names_1[0])
+
+# Add the rest of the names with the insert method
+for i in range(1, len(names_1)):
+    bst.insert(names_1[i])
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# loop over the second list of names using the contains method
+# Searching through a binary search tree significantly decrease the time it takes to find matching names
+
+for i in range(len(names_2)):
+    if bst.contains(names_2[i]):
+        duplicates.append(names_2[i])
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
