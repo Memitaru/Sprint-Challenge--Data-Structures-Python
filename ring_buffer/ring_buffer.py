f81a29108752bd0fb882eacf1707a6e0c2d9c46b
Memitaru <amiezrascott@gmail.com>
2020-02-23 19:33:11 -0800

    Finished ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,7 +8,18 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # Check if the length is equal to the capacity to keep it from going over max capacity
+        # If it's less than capacity just add the item to the tail
+        # If it's equal to the capacity remove the head (oldest value) and add the new one to the tail (a queue)
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        else:
+            to_delete = self.storage.head
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            if to_delete == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
@@ -16,10 +27,29 @@ class RingBuffer:
 
         # TODO: Your code here
 
+        # Start with the current value
+        # If Next is none, use the head instead (this is how the 'ring' works)
+        # Go through the list and as long sa the next node isn't the same as the initial node (or None) append the value
+        initial = self.current
+        list_buffer_contents.append(initial.value)
+
+        if initial.next is not None:
+            next_node = initial.next
+        else:
+            next_node = self.storage.head
+
+        while next_node != initial:
+            list_buffer_contents.append(next_node.value)
+            if next_node.next is not None:
+                next_node = next_node.next
+            else:
+                next_node = self.storage.head
+
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
 
+# MVP can be done with a singly linked list. Since a doubly linked list is imported, will that be useful here?
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
